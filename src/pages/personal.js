51291e88d085ac063113528b61079edea69f7f07
nikoflash <nikoflash@gmail.com>
2020-04-16 15:26:56 +0200

    Change text

--- src/pages/personal.js
+++ src/pages/personal.js
@@ -20,7 +20,7 @@ export default () => {
         <p>
           I live in Elsinore with my wife, who works as a project manager and our daughter. I spend a lot of time playing music and singing in different settings and love to run bare foot in the woods.
         </p>
-        <p>I also spend a lot of time imagining funny website interactions like this site, most of stay in my head, some of them don't make it past Sketch, but I enjoy trying to figure out how to create them.</p>
+        <p>I also spend a lot of time imagining funny website interactions like this site, most of them stay in my head, some of them don't make it past Sketch, but I enjoy trying to figure out how to create them.</p>
       </div>
     </div>
   )
