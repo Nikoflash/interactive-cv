ab51c2f9a799be0a736fdc2ac4dbea31df38868e
nikoflash <nikoflash@gmail.com>
2020-05-07 16:57:33 +0200

    Add mobile cover animation

--- src/components/mobile/cover.js
+++ src/components/mobile/cover.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import styles from './cover.module.scss'
+import {gsap} from 'gsap'
+import {TextPlugin} from 'gsap/TextPlugin'
+gsap.registerPlugin(TextPlugin);
 
 export default () => {
+
+  const div = useRef(null)
+  const name = useRef(null)
+  const dev = useRef(null)
+  const t1 = gsap.timeline({paused: true})
+  
+  useEffect(() => {
+
+    t1
+    .to(div.current, {duration: 0.5, text: "&lt;div&gt;", ease: "none"})
+    .to(name.current, {duration: 1, text: "Nikolas Dalton", ease: "none"})
+    .to(dev.current, {duration: 0.2, text: "&lt;&#47;div&gt;", ease: "none"})
+    .to(dev.current, {duration: 0.5, text: "&lt;&#47;d", ease: "none", delay: 0.5})
+    .to(dev.current, {duration: 0.5, text: "&lt;&#47;dev&gt;", ease: "none"})
+    
+    t1.play()
+  }, [])
+
   return (
     <div className={styles.cover}>
-      <div>
-        <div>
-          &lt;<span>div</span>&gt;
+      <div className={styles.textBox}>
+        <div ref={div} id="div">
+          
         </div>
-        <div className={styles.indent}>
-          <span>Nikolas Dalton</span>
+        <div ref={name} className={styles.indent}>
+          
         </div>
-        <div>
-          &lt;&#47;<span>dev</span>&gt;
+        <div ref={dev} className={styles.dev}>
+          
         </div>
       </div>
     </div>
